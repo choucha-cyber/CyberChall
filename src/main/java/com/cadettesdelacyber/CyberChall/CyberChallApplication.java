aa50affdd1547bb52fe797ea82e6b629bbd9a381
Mimouni <MOI@MacBook-de-Mimouni.local>
2025-04-03 23:13:58 +0200

    Initial commit

---
+++ src/main/java/com/cadettesdelacyber/CyberChall/CyberChallApplication.java
@@ -0,0 +1,13 @@
+package com.cadettesdelacyber.CyberChall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberChallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberChallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadettesdelacyber/CyberChall/CyberChallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadettesdelacyber.CyberChall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberChallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
